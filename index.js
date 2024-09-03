ebf1aa437f4e1586eee5aca4b9e6b5420c8e3900
add <sahilvaidya353@gmail.com>
2024-09-03 17:22:00 +0530

    updated isAdult

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const age = 24;
 const name = 'Sahil';
 const dob = '02/07/2024'
 
-const is Adult = () => {
-    return age >= 18;
+const isAdult = () => {
+    if (a >= 18)
+        return true;
+    else
+        return false;
 
 }
